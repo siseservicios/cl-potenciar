e9e04a5d975fabf286236fc11910aa91d4dbef8a
Jorge Obiols <jobiols@users.noreply.github.com>
2023-05-13 13:08:57 -0300

    Update __manifest__.py

--- potenciar_default/__manifest__.py
+++ potenciar_default/__manifest__.py
@@ -18,7 +18,7 @@
 #
 # -----------------------------------------------------------------------------
 {
-    'name': 'Potenciar13',
+    'name': 'Potenciar1301',
     'version': '13.0.1.0.0',
     'license': 'Other OSI approved licence',
     'category': 'generico',
